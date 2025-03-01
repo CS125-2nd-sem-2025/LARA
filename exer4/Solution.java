b169c1541eda55f51552b5f696c0fd21f3a2f6e5
bryanlara11 <balara1@up.edu.ph>
2025-03-01 10:00:43 +0800

    solution for lab exer 4

---
+++ exer4/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        // Test case
+
+        System.out.println(solution(5,5) == 25); // true
+        System.out.println(solution(10,5) == 50); // true
+        System.out.println(solution(15,12) == 180); // true
+    }
+
+    // Problem
+    // write a recursive function to multiply
+    // two positive integers without using * or /
+    // you can se addition/subtraction and bit shifting
+    // but minimize the number of operations
+    public static int solution(int a, int b) {
+        if (b == 0){
+            return 0;
+        }
+
+        return a + solution(a, b-1);
+    }
+}
